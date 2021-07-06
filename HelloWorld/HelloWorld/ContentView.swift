f2851e86fefe78e8c10d470dc90e86f0f186c431
jamesmin <dev.jamesmin@gmail.com>
2021-07-06 12:02:17 +0900

    add hello world

---
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Jaemin Lee on 2021/07/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
