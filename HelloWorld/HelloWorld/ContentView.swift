fc1303acbfb7b93be33d424dadedcdb2910b5dc2
jamesmin <dev.jamesmin@gmail.com>
2022-10-25 17:10:29 +0900

    프로젝트 셋팅.

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world! why")
-            .font(.headline)
-            .foregroundColor(Color.red)
-            .padding()
+        VStack {
+            Text("Hello, world! why")
+                .font(.title)
+                .foregroundColor(Color.green)
+            Text("Hello, world! why")
+                .font(.title)
+                .foregroundColor(Color.green)
+        }
     }
 }
 
